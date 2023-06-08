e0d32333b284d932f3ed3de019cc97d58d5e5db7
chrzanxq <marcinch199@gmail.com>
2023-06-08 16:06:36 +0200

    pre-final changes

--- js/index.js
+++ js/index.js
@@ -7,4 +7,41 @@
       top: target.offsetTop - offset,
       behavior: "smooth"
     });
+  };
+
+  function sendMail() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const serviceId = "service_voqo5oc";
+    const templateId = "template_q52daaa";
+  
+    if (!name || !email || !message) {
+      return false;
+    }
+  
+    if (!isValidEmail(email)) {
+      return false;
+    }
+  
+    const params = { name, email, message };
+  
+    emailjs
+      .send(serviceId, templateId, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        alert("Message sent successfully!");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("An error occurred while sending the message. Please try again later.");
+      });
   }
+  
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
